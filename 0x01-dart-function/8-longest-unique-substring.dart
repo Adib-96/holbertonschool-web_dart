af9112bbbc174cf077735d4c0292d3e4401d56db
Adib-96 <adibbh1996@gmail.com>
2024-11-07 10:29:30 +0100

    Longest Unique Substring

---
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -0,0 +1,23 @@
+String longestUniqueSubstring(String str) {
+  int length = str.length;
+  int i = 0;
+  List<String> store = [];
+
+  while (i < length) {
+    if (!store.contains(str[i])) {
+      store.add(str[i]);
+    } else {
+      store.remove(str[i]);
+      store.add(str[i]);
+    }
+    i++;
+  }
+
+  return store.join(""); // Output the result
+}
+
+main() {
+  print(longestUniqueSubstring("abcacbd"));
+  print(longestUniqueSubstring("aaaaaaaa"));
+  print(longestUniqueSubstring("abcde"));
+}
