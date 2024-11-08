00f08649a90e30d38a5dc5ac296e6e7f32ef6418
Adib-96 <adibbh1996@gmail.com>
2024-11-08 12:33:09 +0100

    fix code

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -1,31 +1,28 @@
 class User {
-  int? id;
-  String? name;
-  int? age;
-  double? height;
+  int id;
+  String name;
+  int age;
+  double height;
 
-  User({this.id, this.name, this.age, this.height});
+  User(
+      {required this.id,
+      required this.name,
+      required this.age,
+      required this.height});
+
   Map<String, dynamic> toJson() {
-    var details = Map<String, dynamic>();
-    details['name'] = name;
-    details["age"] = age;
-    details["height"] = height;
-    details["id"] = id;
-    return details;
+    return {'id': id, 'name': name, 'age': age, 'height': height};
   }
 
-  //static methode used to create User instance
   static User fromJson(Map<dynamic, dynamic> userJson) {
-    return User(
-      id: userJson['id'],
-      name: userJson['name'],
-      age: userJson['age'],
-      height: userJson['height'],
-    );
+    return new User(
+        id: userJson['id'],
+        name: userJson['name'],
+        age: userJson['age'],
+        height: userJson['height']);
   }
 
-  @override
   String toString() {
-    return "User(id: $id, name: $name, age: $age, height: $height)";
+    return 'User(id : $id ,name: $name, age: $age, height: $height)';
   }
 }
