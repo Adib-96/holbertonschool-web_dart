66b5d570017c21bb4b35608daa66848f12fec0e6
Adib-96 <adibbh1996@gmail.com>
2024-11-08 11:39:17 +0100

    Mutables

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,37 @@
+import 'dart:ffi';
+
+class Password {
+  String? _password;
+  Password({String? password}) {
+    _password = password;
+  }
+
+  String get getterPass {
+    return "Your Password is: $_password";
+  }
+
+  void set password(String password) {
+    _password = password;
+  }
+
+  bool isValid() {
+    if (_password == null) return false;
+    final pw = _password!;
+    final hasMinLength = pw.length >= 8;
+    final hasMaxLength = pw.length <= 16;
+    final hasUppercase = pw.contains(RegExp(r'[A-Z]'));
+    final hasLowercase = pw.contains(RegExp(r'[a-z]'));
+    final hasNumber = pw.contains(RegExp(r'[0-9]'));
+
+    return hasMinLength &&
+        hasMaxLength &&
+        hasUppercase &&
+        hasLowercase &&
+        hasNumber;
+  }
+
+  @override
+  String toString() {
+    return getterPass;
+  }
+}
