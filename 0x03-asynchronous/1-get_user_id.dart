26217f070da94f4bdc76cb113d12a3af31f8dc30
Adib-96 <adibbh1996@gmail.com>
2024-11-29 07:29:56 +0100

    task1 + task2

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,7 +1,18 @@
-import 'dart:convert';
+import '1-util.dart'; // Import the provided util.dart file
 
 Future<String> getUserId() async {
-  var userData = await fetchUserData();
-  var decodeData = jsonDecode(userData);
-  return decodeData["id"];
+  try {
+    String userData = await fetchUserData();
+    final user = jsonDecode(userData);
+    return user['id'];
+  } catch (e) {
+    print('error caught: $e');
+    return 'error caught: $e';
+  }
 }
+
+Future<String> fetchUserData() => Future.delayed(
+      const Duration(seconds: 2),
+      () =>
+          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
+    );
--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -1,7 +1,15 @@
+import '2-util.dart'; // Import the provided util.dart file
+
 Future<void> getUser() async {
   try {
-    String response = await fetchUser();
-  } catch (err) {
-    print("error caught: $err");
+    String user = await fetchUser();
+    print(user);
+  } catch (e) {
+    print('error caught: $e');
   }
 }
+
+Future<String> fetchUser() => Future.delayed(
+      const Duration(seconds: 2),
+      () => throw 'Cannot locate user',
+    );
