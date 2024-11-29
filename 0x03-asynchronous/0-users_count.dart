50f686ee3b45d8a1dff2b82f0760a54fff260669
Adib-96 <adibbh1996@gmail.com>
2024-11-29 07:21:39 +0100

    users count

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,4 +1,15 @@
+import '0-util.dart';
+
 Future<void> usersCount() async {
-  var response = await fetchUsersCount();
-  print(response);
+  try {
+    int count = await fetchUsersCount();
+    print(count);
+  } catch (e) {
+    print('error caught: $e');
+  }
 }
+
+Future<int> fetchUsersCount() => Future.delayed(
+      const Duration(seconds: 2),
+      () => 19,
+    );
